c2c7ae79d3e829d2ca55b7816f6b19dce16018c0
Dirk <dirk@reher.me>
2022-03-17 17:05:53 +0100

    inital

---
+++ coding_task_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coding_task_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
